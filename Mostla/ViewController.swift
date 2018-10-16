579b0f50abd12bd83338482d6b3957f6f38cfbdf
Enrique Barragan <ebarragan1997@gmail.com>
2018-10-16 11:39:22 -0500

    Initial Commit

---
+++ Mostla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mostla
+//
+//  Created by Enrique Barragán on 10/16/18.
+//  Copyright © 2018 Enrique Barragán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
