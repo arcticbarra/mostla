9637322d95834a981df3f2c04e37fb5300d99b74
Oscar Laureano <oscar.laureano.xx@gmail.com>
2018-10-17 19:43:10 -0500

    Add files via upload
    Agregada la info estatica disponible y cada Cell del Collection View te manda a su View con su respectiva info

---
+++ mostla/Mostla/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Mostla
+//
+//  Created by Enrique Barragán on 10/17/18.
+//  Copyright © 2018 Enrique Barragán. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+  @IBOutlet weak var image: UIImageView!
+  @IBOutlet weak var name: UILabel!
+}
